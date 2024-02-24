de6c49cfe4784c1700f5a9d9800af7a02acadc83
Dominicentek <dominicentekgaming@gmail.com>
2024-02-24 01:36:44 +0100

    make the camera scale based on video render viewport instead of game viewport

--- src/game/behaviors/camera.inc.c
+++ src/game/behaviors/camera.inc.c
@@ -1,10 +1,12 @@
 #include "pc/configfile.h"
 #include "pc/gfx/gfx_pc.h"
 
+extern int videores[2];
+
 Gfx* geo_camera_scale(s32 callContext, struct GraphNode *node, UNUSED Mat4 *c) {
     struct GraphNodeScale* scaleNode = (struct GraphNodeScale*)node->next;
     float h = camera_fov / 50.f;
-    vec3f_set(scaleNode->scale, h * gfx_current_dimensions.aspect_ratio, h, 1.f);
+    vec3f_set(scaleNode->scale, h * ((f32)videores[0] / (f32)videores[1]), h, 1.f);
     return NULL;
 }
 
